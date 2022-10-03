1f07fdb12263e2b6a7eddba3dfb43c13b2a2142d
Adrian <adrian.simionescu@rebase.fi>
2022-10-03 15:19:16 +0300

    Removed DB instance creation
    Removed DB instance creation, will be part of specific services. Left only VPC related + general upcoming configs.

--- VirtualFinland.Infrastructure/Stacks/MainStack.cs
+++ VirtualFinland.Infrastructure/Stacks/MainStack.cs
@@ -15,72 +15,23 @@ public class MainStack : Stack
 
         var config = new Config();
         bool isProductionEnvironment = config.Require("environment") == Environments.Prod.ToString().ToLowerInvariant();
-         InputMap<string> tags = new InputMap<string>()
+        InputMap<string> tags = new InputMap<string>()
          {
             { "Environment", config.Require("environment") },
             { "Project", config.Require("name") }
          };
 
-        var vpc = new Vpc($"vf-vpc-{config.Require("environment")}", new VpcArgs() {
-            Tags = tags
-        });
-
-        var dbSubNetGroup = new SubnetGroup("dbsubnets", new()
+        var vpc = new Vpc($"vf-vpc-{config.Require("environment")}", new VpcArgs()
         {
-            SubnetIds = isProductionEnvironment ? vpc.PrivateSubnetIds : vpc.PublicSubnetIds, // DEV: Set a public subnet for dev purposes, otherwise use a prive one for prod
-        });
-
-        var password = new RandomPassword("password", new()
-        {
-            Length = 16,
-            Special = true,
-            OverrideSpecial = "_%@",
-        });
-
-        var rdsCluster = new Cluster($"vf-rds-cluster-{config.Require("environment")}", new()
-        {
-            ClusterIdentifier = $"vf-{config.Require("environment")}",
-
-            EnableHttpEndpoint = !isProductionEnvironment, // DEV: Set to true only for dev purposes for public access non-prod situations
-            Engine = "aurora-postgresql",
-            EngineMode = "provisioned",
-            EngineVersion = "13.7",
-            DatabaseName = config.Require("dbName"),
-            MasterUsername = config.Require("dbAdmin"),
-            MasterPassword = password.Result,
-            SkipFinalSnapshot = !isProductionEnvironment, // DEV: For production set to FALSE to avoid accidental deletion of the cluster, data safety measure and is the default for AWS.
-            DbSubnetGroupName = dbSubNetGroup.Name,
-            Serverlessv2ScalingConfiguration = new ClusterServerlessv2ScalingConfigurationArgs
-            {
-                MaxCapacity = 1,
-                MinCapacity = 0.5,
-            },
-            Tags = tags
-        });
-
-        var rdsClusterInstance = new ClusterInstance($"vf-rds-rdsClusterInstance-{config.Require("environment")}", new()
-        {
-            ClusterIdentifier = rdsCluster.Id,
-            InstanceClass = "db.serverless",
-            Engine = rdsCluster.Engine,
-            EngineVersion = rdsCluster.EngineVersion,
             Tags = tags
         });
 
         this.VpcId = vpc.VpcId;
         this.PublicSubnetIds = vpc.PublicSubnetIds;
         this.PrivateSubnetIds = vpc.PrivateSubnetIds;
-        this.RDSDBInstancePassword = rdsCluster.MasterPassword.Apply(Output.CreateSecret);
-
-        this.RDSClusterEndpoint = rdsCluster.Endpoint;
-        this.RDSClusterVPCSecurityGroups = rdsCluster.VpcSecurityGroupIds;
     }
     [Output] public Output<string> VpcId { get; set; }
 
     [Output] public Output<ImmutableArray<string>> PrivateSubnetIds { get; set; }
     [Output] public Output<ImmutableArray<string>> PublicSubnetIds { get; set; }
-
-    [Output] public Output<string> RDSClusterEndpoint { get; set; }
-    [Output] public Output<string> RDSDBInstancePassword { get; set; }
-    [Output] public Output<ImmutableArray<string>> RDSClusterVPCSecurityGroups { get; set; }
 }
--- VirtualFinland.UnitTests/Tests/MainStackTests.cs
+++ VirtualFinland.UnitTests/Tests/MainStackTests.cs
@@ -33,34 +33,5 @@ namespace VirtualFinland.Infrastructure.Testing.Tests
 
             vpcs.Count.Should().Be(1, "should be a single VPC");
         }
-
-        [Test]
-        public async Task ShouldHaveAuroraServerlessClusterAsync()
-        {
-            var resources = await TestUtility.RunAsync<MainStack>();
-            var dbClusters = resources.OfType<Cluster>().ToList();
-
-            dbClusters.Count().Should().BePositive("should have at least one RDS Cluster");
-            dbClusters.Should().ContainSingle(o => o.Engine.GetValueAsync().Result == "aurora-postgresql");
-        }
-
-        [Test]
-        public async Task ShouldHaveAuroraServerlessInstanceAsync()
-        {
-            var resources = await TestUtility.RunAsync<MainStack>();
-            var dbClustersInstances = resources.OfType<ClusterInstance>().ToList();
-
-            dbClustersInstances.Should().NotBeNull();
-            dbClustersInstances.Should().ContainSingle(o => o.InstanceClass.GetValueAsync().Result == "db.serverless");
-        }
-
-        [Test]
-        public async Task ShouldNotSkipFinalSnapshotForAuroraCluster()
-        {
-            var resources = await TestUtility.RunAsync<MainStack>();
-            var dbClusters = resources.OfType<Cluster>().ToList();
-
-            dbClusters.Should().ContainSingle(o => o.SkipFinalSnapshot.GetValueAsync().Result == false, "should not skip final snapshot for production, safety measure");
-        }
     }
 }
