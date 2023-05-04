4df0751b57c2d3e9c968cbec5396f604c4678014
Lassi Piironen <lsipii@kapsi.fi>
2023-05-04 11:25:37 +0300

    feat: add tagging policies for the most common aws resources

---
+++ policypack/index.ts
@@ -0,0 +1,44 @@
+import * as aws from "@pulumi/aws";
+import { PolicyPack, ReportViolation, validateResourceOfType } from "@pulumi/policy";
+
+new PolicyPack("vfd-aws-policy-pack", {
+  policies: [
+    {
+      name: "require-mandatory-tags",
+      description: "Enforces the use of mandatory resource tags.",
+      enforcementLevel: "mandatory",
+      validateResource: [
+        validateResourceOfType(aws.ec2.Vpc, (resource, args, reportViolation) => {
+          requireMandatoryTags(resource.tags, reportViolation);
+        }),
+        validateResourceOfType(aws.lambda.Function, (resource, args, reportViolation) => {
+          requireMandatoryTags(resource.tags, reportViolation);
+        }),
+        validateResourceOfType(aws.s3.Bucket, (resource, args, reportViolation) => {
+          requireMandatoryTags(resource.tags, reportViolation);
+        }),
+        validateResourceOfType(aws.cloudfront.Distribution, (resource, args, reportViolation) => {
+          requireMandatoryTags(resource.tags, reportViolation);
+        }),
+        validateResourceOfType(aws.rds.Instance, (resource, args, reportViolation) => {
+          requireMandatoryTags(resource.tags, reportViolation);
+        }),
+      ],
+    },
+  ],
+});
+
+function requireMandatoryTags(tags: any, reportViolation: ReportViolation) {
+  const mandatoryTags = ["Environment", "Project"];
+
+  if (typeof tags !== "object" || tags === null) {
+    reportViolation("Missing tags");
+    return;
+  }
+
+  for (const tag of mandatoryTags) {
+    if (!tags[tag]) {
+      reportViolation(`Missing mandatory tag: ${tag}`);
+    }
+  }
+}
