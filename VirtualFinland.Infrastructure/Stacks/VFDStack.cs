c5b005f811c1ebb8a868854d11c3f565b90cf479
Lassi Piironen <lsipii@kapsi.fi>
2023-08-24 15:35:39 +0300

    feat: add a shared access key as a secret stack output

--- VirtualFinland.Infrastructure/Stacks/VFDStack.cs
+++ VirtualFinland.Infrastructure/Stacks/VFDStack.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using Pulumi;
-using VirtualFinland.Infrastructure.Common;
+using Pulumi.Random;
 using VirtualFinland.Infrastructure.Stacks.Features;
 
 namespace VirtualFinland.Infrastructure.Stacks;
@@ -12,9 +12,6 @@ public class VFDStack : Stack
 {
     public VFDStack()
     {
-        var config = new Config();
-
-        bool isProductionEnvironment = Pulumi.Deployment.Instance.StackName == Environments.Prod.ToString().ToLowerInvariant();
         var environment = Pulumi.Deployment.Instance.StackName;
         var projectName = Pulumi.Deployment.Instance.ProjectName;
 
@@ -28,14 +25,26 @@ public class VFDStack : Stack
             }
         };
 
-        var sharedResourceTags = new Dictionary<string, string>(tags) { };
-        sharedResourceTags["vfd:stack"] = "shared";
+        var sharedResourceTags = new Dictionary<string, string>(tags)
+        {
+            ["vfd:stack"] = "shared"
+        };
 
         // Setup key deployer role
         var deployer = new Deployer();
         var deployerRole = deployer.InitializeGitHubOIDCProvider(environment, tags, sharedResourceTags);
-        this.DeployerIAMRole = deployerRole.Arn;
+        DeployerIAMRole = deployerRole.Arn;
+
+        // Setup shared resources
+        var sharedAccessKey = new RandomPassword($"{projectName}-sharedAccessKey-{environment}", new RandomPasswordArgs
+        {
+            Length = 20,
+            Special = false
+        });
+        SharedAccessKey = Pulumi.Output.CreateSecret(sharedAccessKey.Result);
     }
 
     [Output] public Output<string> DeployerIAMRole { get; set; }
+    [Output] public Output<string> SharedAccessKey { get; set; }
+
 }
