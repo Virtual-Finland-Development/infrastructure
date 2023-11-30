269adf72d8026247dbeab9ccd3a717a18e313ac6
Lassi Piironen <lsipii@kapsi.fi>
2023-11-30 11:49:12 +0200

    fix: the oicd provider exists check failed on new installations

--- VirtualFinland.Infrastructure/Program.cs
+++ VirtualFinland.Infrastructure/Program.cs
@@ -1,4 +1,42 @@
-﻿using Pulumi;
-using VirtualFinland.Infrastructure.Stacks;
+﻿using System.Collections.Generic;
+using Pulumi;
+using Pulumi.Random;
+using VirtualFinland.Infrastructure.Stacks.Features;
 
-return await Deployment.RunAsync<VFDStack>();
+return await Deployment.RunAsync(async () =>
+{
+    var environment = Deployment.Instance.StackName;
+    var projectName = Deployment.Instance.ProjectName;
+
+    var tags = new Dictionary<string, string>()
+    {
+        {
+            "vfd:stack", environment
+        },
+        {
+            "vfd:project", projectName
+        }
+    };
+
+    var sharedResourceTags = new Dictionary<string, string>(tags)
+    {
+        ["vfd:stack"] = "shared"
+    };
+
+    // Setup key deployer role
+    var deployer = new Deployer();
+    var deployerRole = await deployer.InitializeGitHubOIDCProviderAsync(environment, tags, sharedResourceTags);
+
+    // Setup shared resources
+    var sharedAccessKey = new RandomPassword($"{projectName}-sharedAccessKey-{environment}", new RandomPasswordArgs
+    {
+        Length = 20,
+        Special = false
+    });
+
+    return new Dictionary<string, object?>
+    {
+        { "DeployerIAMRole", deployerRole.Arn },
+        { "SharedAccessKey", Output.CreateSecret(sharedAccessKey.Result) },
+    };
+});
